e1a1131e8ec357899b7f54f1cc1509139e0b9531
aimanhey <aiman.anderil@gmail.com>
2021-07-09 21:29:43 +0800

    create attendance model

---
+++ attendance/src/main/java/com/example/attendance/model/Attendance.java
@@ -0,0 +1,73 @@
+package com.example.attendance.model;
+
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "attendance")
+public class Attendance {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer attendId;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="date")
+    private Date date;
+
+    @Column(name="image_location")
+    private String image;
+
+
+    public Integer getAttendId(){
+        return attendId;
+    }
+
+    public void setAttendId(Integer attendId){
+        this.attendId=attendId;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public Date getDate(){
+        return date;
+    }
+
+    public void setDate(Date date){
+        this.date=date;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image=image;
+    }
+
+
+
+    
+
+
+
+
+}
